8cbf84349b2ce9cb5c7bd0881df2de675e9e54bb
km-zen <konrad.markowski.1993@gmail.com>
2022-10-19 13:51:01 +0200

    project initialization

---
+++ src/main/java/pl/markowski/konrad/drinkingapp/DrinkingAppApplication.java
@@ -0,0 +1,13 @@
+package pl.markowski.konrad.drinkingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/markowski/konrad/drinkingapp/DrinkingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.markowski.konrad.drinkingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
